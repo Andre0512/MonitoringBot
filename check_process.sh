8cd73c1681b826ba500560a8d9edaafee3c07069
Andre <andre.basche@gmail.com>
2017-07-30 20:08:53 +0200

    Added Bot prototype

--- check_process.sh
+++ check_process.sh
@@ -1,64 +1,5 @@
 #!/bin/bash
 
-function UberspaceProcessi()
-{
-process=$1
-read -d '' -r script <<'ENDSSH'
-    #out=$(pgrep -fl python3)
-    clear
-    pid=$(pgrep -fl python3 | sed 's/.*?\([0-9]\{1,6\}\) python3 '$1'.*/\1/g')
-    echo "$pid"
-    exit
-    if [[ "$pid" =~ ^[0-9]+$ ]]; then
-        echo "Running"
-    else
-        echo "Not running"
-    fi
-    exit
-ENDSSH
-$script
-#echo $(ssh abasche@abasche.de "set -- '$process'; $script")
-}
-
-function UberspaceProcess()
-{
-    test=$(ssh abasche@abasche.de "pgrep -fl python3")
-    pid=$(sed 's/.*?\([0-9]\{1,6\}\) python3 '$1'.*/\1/g' <<< $test)
-    echo $pid 
-}
-
-
-function PiProcess()
-{
-process=$1
-read -d '' -r script <<'ENDSSH'
-    out=$(pgrep -a python3)
-    pid=$(echo $out | sed 's/.*\([0-9]\{1,6\}\) python3 [\/a-zA-Z0-9]*'$1'.*/\1/g')
-    echo $pid
-    exit
-    if [[ "$pid" =~ ^[0-9]+$ ]]; then
-        echo "Running"
-    else
-        echo "Not running"
-    fi
-    exit
-ENDSSH
-echo $(ssh pi@10.5.12.9 "set -- '$process'; $script")
-}
-
-function LocalProcess()
-{
-process=$1
-out=$(pgrep -a python3)
-pid=$(echo $out | sed 's/.*\([0-9]\{1,6\}\) python3 [\/a-zA-Z0-9]*'$1'.*/\1/g')
-echo $(ssh pi@10.5.12.9 "set -- '$process'; $script")
-}
-
-function getPid()
-{
-echo $pid
-}
-
 if [[ "$2" == "Pi" ]];
 then
     result=$(ssh pi@10.5.12.9 "pgrep -a python3")
@@ -73,10 +14,9 @@ pattern='s/\([0-9]\{1,6\}\) [\/a-zA-Z0-9]*python3 [\/a-zA-Z0-9]*'$1'/\1/p'
 pid=$(sed -n "$pattern" <<< $result)
 
 if [[ "$pid" != "" ]]; then
-	state="Running"
+    echo $1"("$pid"): Running"
 else
-	state="Not running"
+    echo $1": Not running"
 fi
 
 
-echo $1"("$pid"): "$state
