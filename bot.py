e4819a7b6b9706cc7142d5f7f874913132276a9e
Andre <andre.basche@gmail.com>
2017-08-16 02:42:25 +0200

    Running updates asynchron

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@
 import yaml
 import subprocess
 import shlex
+from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ParseMode
 import logging
@@ -52,62 +53,65 @@ def start(bot, update):
 def help(bot, update):
     update.message.reply_text('Help!')
 
-
-def update_processes(bot, m_id, msg):
+@run_async
+def update_processes(bot):
     p_dict = cfg['processes']
+    global msg
     for key, value in sorted(p_dict.items()):
         state = subprocess.check_output(shlex.split("bash check_process.sh pgrep " + value + " " + key))
         state = state.decode('utf-8')
         if state.split(" ")[0] == "True":
-            msg = msg.replace("❔ *" + key + "*\n", "✔️ *" + key + "* `" + state.split(" ")[1] + "`")
+            msg.replace("❔ *" + key + "*", "✔️ *" + key + "* (`" + state.split(" ")[1].rstrip("\n") + "`)")
         else:
-            msg = msg.replace("❔ *" + key + "*\n", "❌ *" + key + "*\n")
+            msg.replace("❔ *" + key + "*", "❌ *" + key + "*")
         bot.edit_message_text(chat_id=m_id.chat.id, message_id=m_id.message_id, text=msg, parse_mode=ParseMode.MARKDOWN)
-    return msg 
 
 
-def update_fhem(bot, m_id, msg):
+@run_async
+def update_fhem(bot):
     f_dict = cfg['fhem']
+    global msg
     for key, value in sorted(f_dict.items()):  
         state = subprocess.check_output(shlex.split("bash check_process.sh fhem " + value))
         replace = "✔️" if re.match(".*is running.*", state.decode('utf-8')) else "❌"
-        msg = msg.replace("❔ *" + key + "*\n", replace + " *" + key + "*\n")
+        msg = msg.replace("❔ *" + key + "*", replace + " *" + key + "*")
         bot.edit_message_text(chat_id=m_id.chat.id, message_id=m_id.message_id, text=msg, parse_mode=ParseMode.MARKDOWN)
-    return msg 
 
 
-def update_devices(bot, m_id, msg):
+@run_async
+def update_devices(bot):
     f_dict = cfg['devices']
+    global msg
     for key, value in sorted(f_dict.items()):
         remote = None if not " " in value else value.split(" ")[1]
         state = ping(value.split(" ")[0], remote=remote)
         replace = "✔️" if state else "❌"
-        msg = msg.replace("❔ *" + key + "*\n", replace + " *" + key + "*\n")
+        msg = msg.replace("❔ *" + key + "*", replace + " *" + key + "*")
         bot.edit_message_text(chat_id=m_id.chat.id, message_id=m_id.message_id, text=msg, parse_mode=ParseMode.MARKDOWN)
-    return msg 
 
 
 def add_category(category, msg):
     key_list = sorted(list(cfg[category]))
-    key_list = ["❔ *" + key + "*\n" for key in key_list]
-    msg = msg + "".join(key_list)
+    key_list = ["❔ *" + key + "*" for key in key_list]
+    msg = msg + "\n".join(key_list)
     return msg
 
 
 def base_msg(update):
+    global msg
+    global m_id
     msg = "*Python-Scripts*\n"
-    msg = add_category('processes', msg) + "\n*FHEM*\n"
-    msg = add_category('fhem', msg) + "\n*Geräte*\n"
+    msg = add_category('processes', msg) + "\n\n*FHEM*\n"
+    msg = add_category('fhem', msg) + "\n\n*Geräte*\n"
     msg = add_category('devices', msg)
     m_id = update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
-    return m_id, msg
 
 
 def echo(bot, update):
-    m_id, msg = base_msg(update)
-    msg = update_processes(bot, m_id, msg)
-    msg = update_fhem(bot, m_id, msg)
-    msg = update_devices(bot, m_id, msg)
+    base_msg(update)
+    update_processes(bot)
+    update_fhem(bot)
+    update_devices(bot)
 
 def ping(ip, remote=None):
     add = ['ssh', remote] if remote else []
